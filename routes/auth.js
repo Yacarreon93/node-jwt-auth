ef72488c0ae5275dda0f0674b1c2bc90e4699b56
Yasser Carreón <yacasia@gmail.com>
2021-03-06 18:59:16 -0600

    validate with joi

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,17 @@ const bcrypt = require("bcrypt");
 const router = express.Router();
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+
+const loginSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+});
 
 router.post("/login", async (req, res) => {
   try {
+    const { error } = loginSchema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
     const { email, password } = req.body;
     const user = await User.findOne({ email }).exec();
     if (!user) res.status(401).send("Password or email wrong");
@@ -18,8 +26,15 @@ router.post("/login", async (req, res) => {
   }
 });
 
+const schema = Joi.object({
+  email: Joi.string().required().email(),
+  password: Joi.string().min(6).required(),
+});
+
 router.post("/register", async (req, res) => {
   try {
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
     const { email, password } = req.body;
     const user = await User.findOne({ email }).exec();
     if (user) return res.status(400).send("Email is already taken");
